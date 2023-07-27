e1c46149f75b04a9e8823c60652980a08dbbc67b
Alina Costa <alinacosta87@gmail.com>
2023-07-27 16:32:39 +0300

    updated the project

---
+++ content.md
@@ -0,0 +1,10 @@
+PASI:
+
+- scrii ceva in input si apesi pe buton
+- daca input box-ul este gol va afisa o eroare
+- daca s-a scris ceva si apesi pe buton textul va fi afisat sub input box:
+- fiecare task va avea un buton care va avea ca functionalitate stergerea task-ului
+- daca task-ul este realizat, atunci cand dai click pe el, textul va fi taiat cu o linie si va avea o iconita de tip checked in fata textului
+- daca dai click iar va reveni la stilul initial
+- poti sa salvezi continutul in browser cu localStorage
+  si de cate ori dai refresh la pagina sau inchizi si deschizi browser-ul si accesezi pagina web, lista cu task-uri ramane acolo
--- script.js
+++ script.js
@@ -1,12 +1,18 @@
 const inputBox = document.querySelector('#input-box');
+const errMessage = document.querySelector('.error-message');
+
+const addTask = document.querySelector('.add-task');
 const listContainer = document.querySelector('#list-container');
 
-function addTask() {
+addTask.addEventListener('click', function () {
   if (inputBox.value === '') {
-    alert('You must write something!');
+    errMessage.innerHTML = 'You must write something!';
   } else {
+    errMessage.innerHTML = '';
+    // save the task
     let li = document.createElement('li');
     li.innerHTML = inputBox.value;
+    //this 'li' will ne displayed under this listContainer
     listContainer.appendChild(li);
     let span = document.createElement('span');
     span.innerHTML = '\u00d7';
@@ -14,21 +20,18 @@ function addTask() {
   }
   inputBox.value = '';
   saveData();
-}
+});
 
-listContainer.addEventListener(
-  'click',
-  function (e) {
-    if (e.target.tagName === 'LI') {
-      e.target.classList.toggle('checked');
-      saveData();
-    } else if (e.target.tagName === 'SPAN') {
-      e.target.parentElement.remove();
-      saveData();
-    }
-  },
-  false
-);
+listContainer.addEventListener('click', function (event) {
+  console.log(event);
+  if (event.target.tagName === 'LI') {
+    event.target.classList.toggle('checked');
+    saveData();
+  } else if (event.target.tagName === 'SPAN') {
+    event.target.parentElement.remove();
+    saveData();
+  }
+});
 
 function saveData() {
   localStorage.setItem('data', listContainer.innerHTML);
